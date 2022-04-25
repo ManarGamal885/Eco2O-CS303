2fbb299225ebc7194f6073ff46f59fb3ff09db35
Manar <manarg281@gmail.com>
2022-04-25 21:17:13 +0200

    adding the new structure

---
+++ Eco2O/pages/register.jsx
@@ -0,0 +1,13 @@
+//Rahma
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function register() {
+  return (
+    <View>
+      <Text>register</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ Eco2O/pages/start.jsx
@@ -0,0 +1,13 @@
+//Mabrouk
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function start() {
+  return (
+    <View>
+      <Text>start</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
