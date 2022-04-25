f6573e4af90f7d67dbe9b06d1a7ad7934e932caf
Manar <manarg281@gmail.com>
2022-04-26 00:18:34 +0200

    ''

---
+++ Eco2O/pages/profile.jsx
@@ -0,0 +1,13 @@
+//Manar
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function profile() {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
