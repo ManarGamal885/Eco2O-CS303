abae8ac0822ed5b65abc7aba039295e023e37e3e
Manar <manarg281@gmail.com>
2022-04-26 00:12:52 +0200

    ''

---
+++ Eco2O/pages/forgetPass.jsx
@@ -0,0 +1,14 @@
+//Rahma
+//Rahma note that the forgetpass2 page will be updated at the same page of forgetpass
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function forgetPass() {
+  return (
+    <View>
+      <Text>forgetPass</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
