4cf9ca3737ceac19e04e9d25164a17306c0a499f
Manar <manarg281@gmail.com>
2022-04-26 00:43:18 +0200

    ''

---
+++ Eco2O/pages/suggestionBox.jsx
@@ -0,0 +1,14 @@
+//Manar
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function suggestionBox() {
+  return (
+    <View>
+      <Text>suggestionBox</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+//rnsf
