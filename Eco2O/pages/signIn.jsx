fa838ceec454449dd5d3bbf60b4479cb567f30eb
Manar <manarg281@gmail.com>
2022-04-26 00:36:31 +0200

    ''

---
+++ Eco2O/pages/signIn.jsx
@@ -0,0 +1,13 @@
+//Rahma
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function signIn() {
+  return (
+    <View>
+      <Text>signIn</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
