8d0a4194228c42043e6a08032e7b1021f5cfe0d5
Mohamed Mabrouk <mohamedmbrouk123@gmail.com>
2022-04-27 01:19:00 +0200

    mabrouk add 3 imag  and need image from scane code.

--- Eco2O/App.js
+++ Eco2O/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Scancode from './pages/scancode';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Scancode/>
       <StatusBar style="auto" />
     </View>
   );
--- Eco2O/pages/scancode.jsx
+++ Eco2O/pages/scancode.jsx
@@ -1,13 +1,54 @@
 //Mabrouk
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ImageBackground,
+} from "react-native";
 
-export default function scancode() {
+const Scancode = () => {
   return (
-    <View>
-      <Text>scancode</Text>
-    </View>
+    <ImageBackground
+      source={require("../assets/packground2.png")}
+      style={styles.background}
+    >
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+      <Text style={styles.text} >Building Zero Wast Communities</Text>
+      <ScrollView style={{ padding: 30 }}>
+        <View style={{ padding: 10 }}>
+          <Text>Email</Text>
+        </View>
+
+        <View style={{ paddingLeft: 0 }}>
+          <TextInput placeholder={"example@gmail.com"}></TextInput>
+        </View>
+        <StatusBar style="auto"></StatusBar>
+      </ScrollView>
+    </ImageBackground>
   )
 }
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+  background: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    margin: 20
+  },
+  logo: {
+    height: 150,
+    width: 300,
+    marginTop: 40
+  },
+
+});
+
+export default Scancode;
