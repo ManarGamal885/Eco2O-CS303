c6b4b4597221e5a73a2efd7b8e42eeab4e81d56c
Manar <manarg281@gmail.com>
2022-04-29 22:33:20 +0200

    news page not responsive

--- Eco2O/App.js
+++ Eco2O/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import News from './pages/news'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <News />
     </View>
   );
 }
