fba436ced1a65604124497bcd91c8ae11a9d0dd7
RahmaMandoh <rahmamandoh12@gamil.com>
2022-04-30 22:20:37 +0200

    modified

--- Eco2O/App.js
+++ Eco2O/App.js
@@ -1,37 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './Components/Header';
-import InputFile from './Components/InputFile';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Register from './pages/Register';
 
-export default function App() {
+export default function register() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        {/* Header */}
-        <Header/>
-      </View>
-      <View style={styles.inputs}>
-        {/* Inputs */}
-        <InputFile/>
-      </View>
+    <View>
+      <Register/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  inputs: {
-    justifyContent: 'center',
-    alignItems: 'center',
 
-  }
 });
--- Eco2O/pages/register.jsx
+++ Eco2O/pages/register.jsx
@@ -1,13 +1,69 @@
 //Rahma
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { 
+  StyleSheet, Text, View, Image,
+} from 'react-native';
+import Header from './Components/Header';
+import InputFile from './Components/InputFile';
 
-export default function register() {
+export default function App() {
   return (
-    <View>
-      <Text>register</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        {/* Header */}
+        <View style={styles.headerStyle}>
+          <View style={styles.signUp}>
+            <Text>Sign up</Text>
+          </View>
+
+          <View>
+            <Image
+              source={require('../assets/undraw_connected_world_wuay.png')}
+              style={styles.image}
+            />
+          </View>
+        </View>
+      </View>
+
+
+      <View style={styles.inputs}>
+        {/* Inputs */}
+        <InputFile/>
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerStyle:{
+    flexDirection: 'row',
+  },
+  signUp: {},
+  width: 164,
+  height: 64,
+  fontSize: 40,
+  fontWeight: 'bold',
+  color: '#000000',
+  textShadowColor: '#B7B7B7',
+  textShadowOffset: {width: 0, height: 4},
+  textShadowRadius: 6,
+  justifyContent: 'flex-start',
+  image: {
+
+  },
+  inputs: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }
+});
