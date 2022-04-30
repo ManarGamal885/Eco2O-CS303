ccd982a02b4673b6acfc7d748c6cff44619490e6
RahmaMandoh <rahmamandoh12@gamil.com>
2022-04-30 15:23:46 +0200

    pre-code

--- Eco2O/pages/forgetPass.jsx
+++ Eco2O/pages/forgetPass.jsx
@@ -1,14 +1,44 @@
 //Rahma
 //Rahma note that the forgetpass2 page will be updated at the same page of forgetpass
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../Components/Header';
+import Email from '../Components/Email';
+import Confirm from '../Components/Confirm';
 
-export default function forgetPass() {
+export default function App() {
   return (
-    <View>
-      <Text>forgetPass</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        {/* Header */}
+        <Header/>
+      </View>
+      <View style={styles.email}> 
+        {/* Email  */}
+        <Email/>
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    // paddingTop: 200,
+  },
+  email: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    paddingBottom: 50
+  },
+  // confirm:{
+  //   paddingTop:20,
+  // }
+});
