523f63535979447eff5d4f32d9c5eb01a949bb9f
Mohamed Mabrouk <mohamedmbrouk123@gmail.com>
2022-05-21 15:26:41 +0200

    keybord done

--- Eco2O/pages/Map2.js
+++ Eco2O/pages/Map2.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import {MapView,} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 export default function Map2() {
     return (
